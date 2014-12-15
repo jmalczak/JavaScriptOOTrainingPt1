dd52631be6e427a68abd7adc32d2265f26a0cb4f
Jakub Malczak <j.malczak@samsung.local>
2014-12-15 17:50:17 +0100

    Added package.json

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,7 @@ module.exports = function(config) {
         basePath: '',
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine', 'requirejs'],
+        frameworks: ['jasmine', 'requirejs', 'should'],
         // list of files / patterns to load in the browser
         files: [{
             pattern: 'script/*.js',
--- test/gameLogicSpec.js
+++ test/gameLogicSpec.js
@@ -1,7 +1,14 @@
-define(['gameLogic'], function(gameLogic) {
+define(['gameLogic', 'cell'], function(gameLogic, cell) {
     describe('game logic', function() {
+
+        var logic = new gameLogic();
+
         it('should find winner in one row', function() {
-            expect(true).toEqual(true);
+
+            var cells = [[new cell(0, 0, 'a'), new cell(0, 1, 'a')], [new cell(1, 0, 'v'), new cell(1, 1, 'd')]];
+            var winner = logic.checkWinner(cells);
+            
+            winner.should.be.false;
         });
     });
 });
