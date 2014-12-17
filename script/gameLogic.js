4f7a2da02701031becef2232166e464da84d5954
Jakub Malczak <jmalczak@gmail.com>
2014-12-17 07:55:14 +0100

    New tests

--- script/gameLogic.js
+++ script/gameLogic.js
@@ -2,23 +2,73 @@ define([], function() {
     return function() {
         var self = this;
         self.checkWinner = function(cells) {
-            var height = cells.length;
-            var width = cells[0].length;
-            for (x = 0; x < width; x++) {
-                for (y = 0; y < height; y++) {
-                    if (cells[x][y].isSelected) {
-                        if (self.checkWinnerInternal(cells, x, y)) {
-                            return true;
+            var size = cells.length;
+            var rows = [];
+            var cols = [];
+            var diag1 = [];
+            var diag2 = [];
+            for (x = 0; x < size; x++) {
+                for (y = 0; y < size; y++) {
+
+                    dump(x);
+                    dump(y);
+                    dump(cell[x][y].character);
+
+                    var cell = cells[x][y];
+                    if (rows[x] == undefined || rows[x][cell.character] == undefined) {
+                        var counter = {};
+                        counter[cell.character] = 1;
+                        rows[x] = counter;
+                    } else {
+                        rows[x][cell.character]++;
+                    }
+                    if (cols[y] == undefined || cols[y][cell.character] == undefined) {
+                        var counter = {};
+                        counter[cell.character] = 1;
+                        cols[y] = counter;
+                    } else {
+                        cols[y][cell.character]++;
+                    }
+                    if (x == y) {
+                        if (diag1[x] == undefined) {
+                            var counter = {};
+                            counter[cell.character] = 1;
+                            diag1[x] = counter;
+                        } else {
+                            diag1[x]++;
                         }
+                        if (diag1[x][cell.character] == size) {
+                            return cell.character;
+                        }
+                    }
+                    if (x + y == size) {
+                        if (diag2[x] == undefined) {
+                            var counter = {};
+                            counter[cell.character] = 1;
+                            diag2[x] = counter;
+                        } else {
+                            diag2[x]++;
+                        }
+                        if (diag2[x][cell.character] == size) {
+                            return cell.character;
+                        }
+                    }
+                    if (rows[x][cell.character] == size) {
+                        return cell.character;
+                    }
+                    if (cols[y][cell.character] == size) {
+                        return cell.character;
                     }
                 }
             };
-            return false;
+
+            dump(diag2);
+
+            return null;
         };
         self.checkWinnerInternal = function(cells, startFromX, startFromY, previousCount) {
             var height = cells.length;
             var width = cells[0].length;
-            
             if (previousCount == width) {
                 return true;
             } else if (startFromY >= height || startFromX >= width) {
--- test/gameLogicSpec.js
+++ test/gameLogicSpec.js
@@ -1,14 +1,29 @@
 define(['gameLogic', 'cell'], function(gameLogic, cell) {
     describe('game logic', function() {
-
         var logic = new gameLogic();
-
         it('should find winner in one row', function() {
-
-            var cells = [[new cell(0, 0, 'a'), new cell(0, 1, 'a')], [new cell(1, 0, 'v'), new cell(1, 1, 'd')]];
+            var cells = [
+                [new cell(0, 0, 'a'), new cell(0, 1, 'a')],
+                [new cell(1, 0, 'v'), new cell(1, 1, 'd')]
+            ];
             var winner = logic.checkWinner(cells);
-            
-            winner.should.be.false;
+            winner.should.be.exactly('a');
+        });
+        it('should find winner in one column', function() {
+            var cells = [
+                [new cell(0, 0, 'x'), new cell(0, 1, 'v')],
+                [new cell(1, 0, 'x'), new cell(1, 1, '')]
+            ];
+            var winner = logic.checkWinner(cells);
+            winner.should.be.exactly('x');
+        });
+        it('should find winner in diag 1', function() {
+            var cells = [
+                [new cell(0, 0, 'x'), new cell(0, 1, 'v')],
+                [new cell(1, 0, 's'), new cell(1, 1, 'x')]
+            ];
+            var winner = logic.checkWinner(cells);
+            winner.should.be.exactly('x');
         });
     });
 });
